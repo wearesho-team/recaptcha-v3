4e63013e492539040b3589d4e4b66e926354a8a8
Alexander Letnikow <reclamme@gmail.com>
2018-06-20 23:16:35 +0300

    Test Response entity

---
+++ tests/ResponseTest.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace Wearesho\ReCaptcha\V3\Tests;
+
+use PHPUnit\Framework\TestCase;
+use Wearesho\ReCaptcha;
+
+/**
+ * Class ResponseTest
+ * @package Wearesho\ReCaptcha\V3\Tests
+ */
+class ResponseTest extends TestCase
+{
+    const SUCCESS = true;
+    const SCORE = 0.6;
+    const ACTION = 'login';
+    const TIMESTAMP = 190000;
+    const HOSTNAME = 'wearesho.com';
+    const ERROR_CODE = 'error-code';
+
+    /** @var ReCaptcha\V3\Response */
+    protected $response;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->response = new ReCaptcha\V3\Response(
+            static::SUCCESS,
+            static::SCORE,
+            static::ACTION,
+            (new \DateTime())->setTimestamp(static::TIMESTAMP),
+            static::HOSTNAME,
+            [
+                static::ERROR_CODE,
+            ]
+        );
+    }
+
+    public function testIsSuccess(): void
+    {
+        $this->assertEquals(
+            static::SUCCESS,
+            $this->response->isSuccess()
+        );
+    }
+
+    public function testGetScore(): void
+    {
+        $this->assertEquals(
+            static::SCORE,
+            $this->response->getScore()
+        );
+    }
+
+    public function getGetAction(): void
+    {
+        $this->assertEquals(
+            static::ACTION,
+            $this->response->getAction()
+        );
+    }
+
+    public function testGetDateTime(): void
+    {
+        $this->assertEquals(
+            static::TIMESTAMP,
+            $this->response->getDateTime()->getTimestamp()
+        );
+    }
+
+    public function testGetHostName(): void
+    {
+        $this->assertEquals(
+            static::HOSTNAME,
+            $this->response->getHostname()
+        );
+    }
+
+    public function testGetErrors(): void
+    {
+        $this->assertEquals([
+            static::ERROR_CODE,
+        ], $this->response->getErrors());
+    }
+}
