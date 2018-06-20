c352e0eedeb69fcf94004a156ea83eb0f5a118ad
Alexander Letnikow <reclamme@gmail.com>
2018-06-20 23:06:43 +0300

    Test Config entity

---
+++ tests/ConfigTest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Wearesho\ReCaptcha\V3\Tests;
+
+use PHPUnit\Framework\TestCase;
+use Wearesho\ReCaptcha;
+
+/**
+ * Class ConfigTest
+ * @package Wearesho\ReCaptcha\V3\Tests
+ */
+class ConfigTest extends TestCase
+{
+    const SECRET = 'testSecret';
+
+    /** @var ReCaptcha\V3\Config */
+    protected $config;
+
+    protected function setUp()
+    {
+        parent::setUp();
+        $this->config = new ReCaptcha\V3\Config(static::SECRET);
+    }
+
+    public function testGetSecret()
+    {
+        $this->assertEquals(
+            static::SECRET,
+            $this->config->getSecret()
+        );
+    }
+}
